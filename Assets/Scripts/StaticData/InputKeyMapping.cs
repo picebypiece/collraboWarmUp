44bcb28b83bd32ed7d9e3a552edf0e4a8c6b824a
TU choi <chlx357@gmail.com>
2019-12-12 21:02:00 +0900

    인풋 매니저 키맵핑 준비, 스크립트 선처리 에셋이 깃에서 무시되지 않는거 수정

---
+++ Assets/Scripts/StaticData/InputKeyMapping.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 작성일자 : 2019-12-12-PM-8-33
+// 작성자   : 최태욱
+// 간단설명 : 게임에 사용하는 키의 종류
+
+public static class SDInputKeyMapping 
+{
+    //기본 방향키 UI조작, 기본 키 입력
+    public static string ArrowUp = "ARROW_UP";
+    public static string ArrowDown = "ARROW_DOWN";
+    public static string ArrowRight = "ARROW_RIGHT";
+    public static string ArrowLeft = "ARROW_LEFT";
+    public static string Escape = "Escape";
+
+
+    //인게임에서 사용되는 키입력
+    public static string GameUp = "GAME_UP";
+    public static string GameDown = "GAME_DOWN";
+    public static string GameRight = "GAME_RIGHT";
+    public static string GameLeft = "GAME_LEFT";
+    public static string Jump = "JUMP";
+
+}
