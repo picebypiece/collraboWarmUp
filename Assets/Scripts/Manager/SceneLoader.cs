331c5b5b996530a1569a729ffda085801b1a5b90
SilverMwi <chlx357@gmail.com>
2020-01-11 23:31:30 +0900

    씬로더 초벌작업

---
+++ Assets/Scripts/Manager/SceneLoader.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// 작성일자 : 2020-01-11-PM-10-48
+// 작성자  : 최태욱
+// 간단설명 : 씬 로드 관리
+
+public class SceneLoader : SingletonManager<SceneLoader>
+{
+
+    // Variable
+    #region Variable
+
+    #endregion
+
+    // Property
+    #region Property
+    public Scene PrevScene { get; private set; }
+    public Scene CurrentScene { get; private set; }
+    #endregion
+
+    // MonoBehaviour
+    #region MonoBehaviour
+    void Awake()
+    {
+        CurrentScene = PrevScene = SceneManager.GetActiveScene();
+        SceneManager.sceneLoaded += SceneLoaded;
+    }
+
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= SceneLoaded;
+    }
+    #endregion
+
+    // Private Method
+    #region Private Method
+
+    void SceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        PrevScene = CurrentScene;
+        CurrentScene = scene;
+    }
+    #endregion
+
+    // Public Method
+    #region Public Method
+
+    public void LoadScene(string SceneName, LoadSceneMode sceneMode = LoadSceneMode.Single)
+    {
+        SceneManager.LoadScene(SceneName, sceneMode);
+    }
+
+    
+    #endregion
+}
+
